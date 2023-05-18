6c86a7ab6b73ab97ecfe0cc5ae42aa30436565ec
Griswalde~ <agunggriswald158@gmail.com>
2023-05-18 22:13:59 +0700

    first commit

---
+++ app/src/main/java/id/ac/unpas/functionalcompose/networks/SetoranSampahGetResponse.kt
@@ -0,0 +1,8 @@
+package id.ac.unpas.functionalcompose.networks
+
+import id.ac.unpas.functionalcompose.model.SetoranSampah
+
+data class SetoranSampahGetResponse(
+    val data: List<SetoranSampah>? = null
+)
+
